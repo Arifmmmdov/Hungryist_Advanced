9a184bbb5224b1cebe32eab6ec72bb2634d45910
User6 <user6@expressbank.edu>
2022-08-29 15:01:05 +0400

    [master] Splash screen completed

---
+++ app/src/main/java/com/example/hungryist/fragment/SplashFragment.kt
@@ -0,0 +1,44 @@
+package com.example.hungryist.fragment
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import com.example.hungryist.R
+import com.example.hungryist.databinding.FragmentSplashBinding
+
+class SplashFragment(millisInFuture: Long) : Fragment() {
+    private val binding by lazy {
+        FragmentSplashBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        object : CountDownTimer(2000,1000) {
+            override fun onTick(p0: Long) {
+            }
+
+            override fun onFinish() {
+                //TODO Move to login or main page
+                val fragmentManager: FragmentManager = parentFragmentManager
+                fragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container,LoginOrRegisterFragment::class.java,null)
+                    .addToBackStack("")
+                    .commit()
+
+            }
+
+        }
+    }
+
+}
